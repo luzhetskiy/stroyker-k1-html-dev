96e597c28926f3218ee6c93dbfd01c518a137321
unknown <nikitos.gg.yt@gmail.com>
2023-08-09 17:33:21 +0400

    feat: add base arhitecture

---
+++ src/app/index.js
@@ -0,0 +1,2 @@
+import './styles/index.sass'
+import '../pages/index.js'
--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
-import './styles/index.sass'
-import './html/pages/index.html'
-import './html/pages/article.html'
+import './app/index.js'
+import './pages/index.js'
