e152131d0da20efff8628c9d2095036fe9fa9c16
unknown <nikitos.gg.yt@gmail.com>
2023-08-30 09:39:22 +0400

    feat: add pages with headers

--- webpack.config.js
+++ webpack.config.js
@@ -126,7 +126,7 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
         generator: {
-          filename: "images/[name][ext][query]", // [hash]
+          filename: "images/[hash][ext][query]", // [hash]
         },
       },
       {
