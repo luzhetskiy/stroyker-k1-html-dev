9b1d269a1f325c43e015a91b0e2a61ea6090c734
Cobalt-A <nikitos.gg.yt@gmail.com>
2024-02-13 16:06:45 +0400

    fix: global styles

--- src/app/scripts/headers-custom.js
+++ src/app/scripts/headers-custom.js
@@ -1,1 +1,31 @@
 //@include ../../components/scripts/headers-custom.js
+
+$(document).ready(() => {
+  // phone toggle
+  document.addEventListener("click", function (e) {
+    if (!e.target.matches(".contact-box p")) return;
+    else {
+      e.target.classList.add("toggled");
+    }
+  });
+
+  // catalog and menu vars
+  var catalogBtn = document.querySelector(".catalog-btn");
+  var catalogBox = document.querySelector(".catalog-box");
+  var menuBtn = document.querySelector(".menu-btn");
+  var menuBox = document.querySelector(".menu-box");
+
+  // catalog open
+  document.addEventListener("click", function (e) {
+    if (e.target.matches(".catalog-btn")) {
+      catalogBtn.classList.toggle("toggled");
+      catalogBox.classList.toggle("show");
+      if (menuBtn) {
+        menuBtn.classList.remove("toggled");
+        menuBox.classList.remove("show");
+      }
+    } else {
+      return;
+    }
+  });
+});
