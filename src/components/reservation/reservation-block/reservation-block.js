411719fa627ac958f39e1eea0f10b1ee379f159c
Cobalt-A <nikitos.gg.yt@gmail.com>
2024-02-29 15:15:24 +0400

    feat: add booking

--- src/components/reservation/reservation-block/reservation-block.js
+++ src/components/reservation/reservation-block/reservation-block.js
@@ -1,45 +1,77 @@
 $(() => {
-  const selectedHours = []
-  const tooltipContent = $('[data-reservation-tooltip]')
+  const selectedItems = new Map();
+  const tooltipContent = $("[data-booking-tooltip]");
 
-  const reservationButtonClickHandler = (event) => {
-    const target = $(event.currentTarget)
-    const time = target.attr('data-time')
-    const title = target.attr('data-reservation-title')
-    const value = `Хочу забронировать ${title} на время на время ${time}`
+  const getValues = (map) => {
+    let result = "";
+    map.forEach((element) => {
+      if (result) result += ",";
+      result += ` ${element.title} на время ${element.time}`;
+    });
+    return result;
+  };
 
-    if ($('.custom-form-instance').length) {
-      const form = $('.custom-form-instance')
-      if (!form.length) return
-      form.find('textarea').val(value)
-      $('html, body').animate({
-          scrollTop: form.offset().top
-      }, 1000);
-      return
+  const bookingButtonClickHandler = (event) => {
+    const value = `Хочу забронировать` + getValues(selectedItems);
+
+    if ($(".custom-form-instance").length) {
+      const form = $(".custom-form-instance");
+      if (!form.length) return;
+      form.find("textarea").val(value);
+      $("html, body").animate(
+        {
+          scrollTop: form.offset().top,
+        },
+        1000
+      );
+      return;
     }
 
-    const form = $('#feedback-message-request-form')
-    if (!form.length) return
-    form.find('textarea').val(value)
-    $('html, body').animate({
-        scrollTop: form.offset().top
-    }, 1000);
-  }
+    const form = $("#feedback-message-request-form");
+    if (!form.length) return;
+    form.find("textarea").val(value);
+    $("html, body").animate(
+      {
+        scrollTop: form.offset().top,
+      },
+      1000
+    );
+  };
 
-  
-  tippy('[data-reservation-tooltip]', {
-    content: tooltipContent.html(),
-    allowHTML: true,
-    placement: 'top-start',
-    theme: 'light',
-    interactive: true,
-    onShown(instance) {
-      $('[data-reservation-button]').on('click', reservationButtonClickHandler)
-    },
-    onHide(instance) {
-      $('[data-reservation-button]').off('click', reservationButtonClickHandler)
-    },
+  $("[data-booking-tooltip]").on("click", (event) => {
+    const target = $(event.currentTarget);
+    const title = target.parent().parent().find("[data-booking-title]").text();
+    const time = target.attr("data-booking-time");
+    const value = title + time
+    const item = {
+      time: time,
+      title: title,
+    };
+    if (selectedItems.get(value)) {
+      selectedItems.delete(value);
+      target.removeAttr("data-booking-item-selected");
+      return;
+    }
+    selectedItems.set(value, item);
+    target.attr("data-booking-item-selected", "");
   });
 
-})
-
+  tippy("[data-booking-tooltip]", {
+    content: tooltipContent.html(),
+    allowHTML: true,
+    placement: "top-start",
+    theme: "light",
+    interactive: true,
+    onMount(instance) {
+      const target = $(instance.reference);
+      const title = target.parent().parent().find("[data-booking-title]").text();
+      const time = target.attr("data-booking-time");
+      $("[data-booking-tooltip-button]").on("click", bookingButtonClickHandler);
+      $("[data-booking-tooltip-title]").text(title);
+      $("[data-booking-tooltip-time]").text(time);
+    },
+    onHide(instance) {
+      $("[data-booking-tooltip-button]").off("click", bookingButtonClickHandler);
+    },
+  });
+});
