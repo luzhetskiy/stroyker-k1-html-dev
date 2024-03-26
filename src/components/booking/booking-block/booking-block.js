a0697da842ceb8c74feb2495fc9ff9becb2f5d9a
vituuushka <usmanova.vita13@gmail.com>
2024-03-26 17:30:03 +0400

    refactor: refactor date in booking comment

--- src/components/booking/booking-block/booking-block.js
+++ src/components/booking/booking-block/booking-block.js
@@ -4,7 +4,7 @@ $(() => {
   const tooltipContent = $("[data-booking-tooltip-content]");
   const clonedTooltipContent = tooltipContent.clone();
   tooltipContent.remove();
-  
+
   tippy("[data-booking-tooltip]", {
     content: clonedTooltipContent.html(),
     allowHTML: true,
@@ -14,51 +14,62 @@ $(() => {
     delay: [100, 200],
     onMount(instance) {
       const target = $(instance.reference);
-      const title = target.parent().parent().find("[data-booking-title]").attr("data-booking-title");
+      const title = target
+        .parent()
+        .parent()
+        .find("[data-booking-title]")
+        .attr("data-booking-title");
       const time = target.attr("data-booking-time");
       const action = target.attr("data-submit-url");
       const button = $("[data-booking-tooltip-button]");
-      
-        // const url = 'https://secret-smr.ru/booking/matrix/test-hours/2024-03-26/'
-        var url = window.location.href;
-        var pattern = /(\d{4})-(\d{2})-(\d{2})/;
-        var match = url.match(pattern);
-        
-        if (match) {
-            var year = match[1];
-            var month = match[2];
-            var day = match[3];
-            var date = day + '.' + month + '.' + year;
-            console.log(date);
-        } 
+
+      // const url = "https://secret-smr.ru/booking/matrix/test-hours/2024-03-26/";
+      const url = window.location.pathname;
+      const pattern = /(\d{4})-(\d{2})-(\d{2})/;
+      const match = url.match(pattern);
+      let date = "";
+      if (match) {
+        const year = parseInt(match[1], 10);
+        const month = parseInt(match[2], 10) - 1;
+        const day = parseInt(match[3], 10);
+        const formattedDate = new Date(year, month, day);
+        date = formattedDate.toLocaleDateString();
+      }
       button.attr("data-booking-tooltip-button-time", time);
       button.attr("data-booking-tooltip-button-title", title);
       $("[data-booking-tooltip-title]").text(title);
       $("[data-booking-tooltip-time]").text(time);
       $("[data-booking-tooltip-button]").on("click", (event) => {
         const form = $("#booking-form");
-        $("[data-booking-item-selected]").removeAttr("data-booking-item-selected");
+        $("[data-booking-item-selected]").removeAttr(
+          "data-booking-item-selected"
+        );
         target.attr("data-booking-item-selected", "");
 
         if (!form.length) return;
         form.find("input, textarea, button").removeAttr("disabled");
         form.attr("action", action);
-        if(date) {
-        form.find('[name="message"]').val(`Хочу забронировать: ${title}, дата: ${date}, время: ${time}`);
-        $("html, body").animate(
-          {
-            scrollTop: $("[data-booking-form]").offset().top - 200,
-          },
-          500
-        );
-        } else {
-          form.find('[name="message"]').val(`Хочу забронировать: ${title}, время: ${time}`);
-        $("html, body").animate(
-          {
-            scrollTop: $("[data-booking-form]").offset().top - 200,
-          },
-          500
-        );
+        if (date) {
+          form
+            .find('[name="message"]')
+            .val(`Хочу забронировать: ${title}, дата: ${date}, время: ${time}`);
+          $("html, body").animate(
+            {
+              scrollTop: $("[data-booking-form]").offset().top - 200,
+            },
+            500
+          );
+        }
+        if (!date) {
+          form
+            .find('[name="message"]')
+            .val(`Хочу забронировать: ${title}, время: ${time}`);
+          $("html, body").animate(
+            {
+              scrollTop: $("[data-booking-form]").offset().top - 200,
+            },
+            500
+          );
         }
       });
     },
@@ -67,10 +78,15 @@ $(() => {
   $("[data-booking-scroll]").scroll((event) => {
     const currentScroll = $(event.currentTarget).scrollLeft();
     if (currentScroll > 1) {
-      $("[data-booking-mobile-titles]").attr("data-booking-mobile-titles-active", "");
+      $("[data-booking-mobile-titles]").attr(
+        "data-booking-mobile-titles-active",
+        ""
+      );
       return;
     }
-    $("[data-booking-mobile-titles-active]").removeAttr("data-booking-mobile-titles-active");
+    $("[data-booking-mobile-titles-active]").removeAttr(
+      "data-booking-mobile-titles-active"
+    );
   });
 
   const appendMobileTitles = () => {
