aa32ef12c30cb3558e1c8979d8ba81ddf806a636
vituuushka <usmanova.vita13@gmail.com>
2024-05-22 16:50:32 +0400

    fix: tires

--- src/components/tire-filter/tire-filter.js
+++ src/components/tire-filter/tire-filter.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     ".filter-toggle .toggle-input"
   );
 
-  if (!tireFilterToggle) return
+  if (!tireFilterToggle) return;
 
   tireFilterToggle.addEventListener("change", function () {
     let blockParameter = document.querySelector(".parameter-block");
@@ -23,12 +23,10 @@ $(document).ready(function () {
     poAuto.style.color = "#A7A7AB";
   });
   function resetFilters(block) {
-    let selectors = block.querySelectorAll(
-      ".parameter-select .selector"
-    )
-    let resetButton = block.querySelector(".button-reset")
-    resetButton.style.display = "none"
-    
+    let selectors = block.querySelectorAll(".parameter-select .selector");
+    let resetButton = block.querySelector(".button-reset");
+    resetButton.style.display = "none";
+
     selectors.forEach(function (selector) {
       let selectedOption = selector.querySelector(".selected-option");
       let selectOptions = selector.querySelector(".select-options");
@@ -55,7 +53,7 @@ $(document).ready(function () {
   }
 
   // search selector
-  
+
   function toggleOptions(searchInput, selectOptions, selectedOption) {
     selectOptions.style.display =
       selectOptions.style.display === "none" ? "block" : "none";
@@ -76,47 +74,52 @@ $(document).ready(function () {
     const text = selectOption.textContent.toLowerCase();
     selectOption.style.display = text.includes(searchString) ? "block" : "none";
   }
-  
+
   const selectors = document.querySelectorAll(".selector");
 
   function checkSelectors() {
-    const atLeastOneSelectedParam = Array.from(selectors).some(function(selector) {
+    const atLeastOneSelectedParam = Array.from(selectors).some(function (
+      selector
+    ) {
       return selector.querySelector(".parameter-block .selected") !== null;
     });
-    atLeastOneSelectedParam ? resetFilterButtonParameter.style.display = "flex" :
-    resetFilterButtonParameter.style.display = "none"
-    const atLeastOneSelectedAuto = Array.from(selectors).some(function(selector) {
+    atLeastOneSelectedParam
+      ? (resetFilterButtonParameter.style.display = "flex")
+      : (resetFilterButtonParameter.style.display = "none");
+    const atLeastOneSelectedAuto = Array.from(selectors).some(function (
+      selector
+    ) {
       return selector.querySelector(".auto-block .selected") !== null;
     });
-    atLeastOneSelectedAuto ? resetFilterButtonAuto.style.display = "flex" :
-    resetFilterButtonAuto.style.display = "none"
-    
+    atLeastOneSelectedAuto
+      ? (resetFilterButtonAuto.style.display = "flex")
+      : (resetFilterButtonAuto.style.display = "none");
   }
 
   selectors.forEach(function (selector) {
-    selector.addEventListener("click", function(event) {
+    selector.addEventListener("click", function (event) {
       const target = event.target;
       if (target !== this.firstChild) {
         checkSelectors();
       }
-    })
+    });
     const selectedOption = selector.querySelector(".selected-option");
     const searchInput = selector.querySelector(".select-search-input");
     const selectOptions = selector.querySelector(".select-options");
-    selectedOption.style.display = "block"
-    searchInput.style.display = "none"
-    selectOptions.style.display = "none"
+    selectedOption.style.display = "block";
+    searchInput.style.display = "none";
+    selectOptions.style.display = "none";
     selectedOption.addEventListener("click", function () {
       toggleOptions(searchInput, selectOptions, selectedOption);
     });
     const selectOption = selector.querySelectorAll(".select-option");
     selectOption.forEach(function (selectOption) {
       selectOption.addEventListener("click", function (event) {
-        const target = event.target
-        if(target !== selector.firstChild) {
-          const selected = selector.querySelector('.selected')
-          selected ? selected.classList.remove("selected"): null
-          target.classList.add("selected")
+        const target = event.target;
+        if (target !== selector.firstChild) {
+          const selected = selector.querySelector(".selected");
+          selected ? selected.classList.remove("selected") : null;
+          target.classList.add("selected");
         }
         selectedOption.textContent = selectOption.textContent;
         closeOptions(searchInput, selectOptions, selectedOption);
@@ -136,7 +139,6 @@ $(document).ready(function () {
     });
   });
 
-
   // слайдер категорий в мобилке
 
   $(".tabs-slider-mobile").slick({
@@ -148,35 +150,34 @@ $(document).ready(function () {
     centerMode: false,
   });
 
+  const tabs = document.querySelectorAll(".tab-content");
   if (document.documentElement.clientWidth < 768) {
-    if (document.documentElement.clientWidth < 768) {
-      const tabs = document.querySelectorAll(".tab-content");
-      tabs.forEach(function (tab) {
-        tab.style.display = "none";
-      });
-    }
+    tabs.forEach(function (tab) {
+      tab.style.display = "none";
+    });
   }
 
   const sliderItems = document.querySelectorAll(
     ".tabs-slider-mobile .slider-item"
   );
-
-  sliderItems.forEach(function (item) {
-    item.addEventListener("click", function (event) {
-      const targetItem = event.target
-      const tabId = item.getAttribute("data-tab");
-      const tab = document.getElementById(tabId);
-      const allTabs = document.querySelectorAll(".tab-content");
-      allTabs.forEach(function (tab) {
-        tab.style.display = "none";
+  if (document.documentElement.clientWidth < 768) {
+    sliderItems.forEach(function (item) {
+      item.addEventListener("click", function (event) {
+        const targetItem = event.target;
+        const tabId = item.getAttribute("data-tab");
+        const tab = document.getElementById(tabId);
+        const allTabs = document.querySelectorAll(".tab-content");
+        allTabs.forEach(function (tab) {
+          tab.style.display = "none";
+        });
+        tab.style.display = "block";
+        const mobileSlider = document.querySelector(".tabs-slider-mobile");
+        const itemActive = mobileSlider.querySelector(".slider-item.active");
+        itemActive ? itemActive.classList.remove("active") : null;
+        targetItem.classList.add("active");
       });
-      tab.style.display = "block";
-      const mobileSlider = document.querySelector(".tabs-slider-mobile")
-      const itemActive = mobileSlider.querySelector(".slider-item.active")
-      itemActive ? itemActive.classList.remove("active") : null
-      targetItem.classList.add("active")
     });
-  });
+  }
 
   // всплывашка
 
