e9985d914ebb6d15b6ac73cfa68c680a98367889
Andrey Mayorov <aamayorov1@gmail.com>
2025-06-25 08:44:37 +0400

    task #14437

--- commerce/js/cart-simple.js
+++ commerce/js/cart-simple.js
@@ -3,21 +3,40 @@
       let pickupSection = document.getElementById("pickup-section");  // pickup section
 
       function toggleExtraFields() {
+          // Проверяем, существует ли pickupSection
+          if (pickupSection) {
+              let delivery_selected = document.querySelector('input[name="delivery_variant"]:checked');
+              let payment_selected = document.querySelector('input[name="payment_variant"]:checked');
+
+              if (!delivery_selected || !payment_selected) {
+                  return; // если не выбраны варианты, ничего не делаем
+              }
+
+              let delivery_id = parseInt(delivery_selected.getAttribute("id").slice(-1));
+              let payment_id = parseInt(payment_selected.getAttribute("id").slice(-1));
+
+              switch (delivery_id) {
+                  case 0:
+                      pickupSection.style.display = "block";
+                      break;
+                  case 1:
+                  case 2:
+                      pickupSection.style.display = "none";
+                      break;
+              }
+          }
+          // Если pickupSection нет, просто пропускаем работу с ним
+
+          // Продолжаем выполнять остальные действия
           let delivery_selected = document.querySelector('input[name="delivery_variant"]:checked');
           let payment_selected = document.querySelector('input[name="payment_variant"]:checked');
+
+          if (!delivery_selected || !payment_selected) {
+              return; // если не выбраны варианты, ничего не делаем
+          }
+
           let delivery_id = parseInt(delivery_selected.getAttribute("id").slice(-1));
           let payment_id = parseInt(payment_selected.getAttribute("id").slice(-1));
-          switch (delivery_id) {
-              case 0:
-                  pickupSection.style.display = "block";
-                  break;
-              case 1:
-                  pickupSection.style.display = "none";
-                  break;
-              case 2:
-                  pickupSection.style.display = "none";
-                  break;
-          }
 
           $('.cart-extra-field').each(function (index, element) {
               let element_bind = parseInt(element.getAttribute("binded"));
@@ -26,8 +45,9 @@
               } else {
                   $(element).hide();
               }
-          })
+          });
       }
+
       $("input[name=delivery_variant]").click(toggleExtraFields);
       $("input[name=payment_variant]").click(toggleExtraFields);
       toggleExtraFields();
