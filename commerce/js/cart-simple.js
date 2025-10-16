b09b122925e6727f7131f7f04e2c9383575281e2
Andrey Mayorov <aamayorov1@gmail.com>
2025-10-16 13:18:43 +0400

    task 14437 fix

--- commerce/js/cart-simple.js
+++ commerce/js/cart-simple.js
@@ -1,55 +1,27 @@
 (function ($) {
   $(document).ready(function () {
-      let pickupSection = document.getElementById("pickup-section");  // pickup section
+    var delivery_payment_mapping = JSON.parse(document.getElementById("delivery_payment_mapping").textContent);
+    var current_delivery = $("input[name=delivery_variant]:checked").val();
+    var payment_block = $("#id_payment_variant").parent();
 
-      function toggleExtraFields() {
-          // Проверяем, существует ли pickupSection
-          if (pickupSection) {
-              let delivery_selected = document.querySelector('input[name="delivery_variant"]:checked');
-              let payment_selected = document.querySelector('input[name="payment_variant"]:checked');
-
-              if (!delivery_selected || !payment_selected) {
-                  return; // если не выбраны варианты, ничего не делаем
-              }
-
-              let delivery_id = parseInt(delivery_selected.getAttribute("id").slice(-1));
-              let payment_id = parseInt(payment_selected.getAttribute("id").slice(-1));
-
-              switch (delivery_id) {
-                  case 0:
-                      pickupSection.style.display = "block";
-                      break;
-                  case 1:
-                  case 2:
-                      pickupSection.style.display = "none";
-                      break;
-              }
-          }
-          // Если pickupSection нет, просто пропускаем работу с ним
-
-          // Продолжаем выполнять остальные действия
-          let delivery_selected = document.querySelector('input[name="delivery_variant"]:checked');
-          let payment_selected = document.querySelector('input[name="payment_variant"]:checked');
-
-          if (!delivery_selected || !payment_selected) {
-              return; // если не выбраны варианты, ничего не делаем
-          }
-
-          let delivery_id = parseInt(delivery_selected.getAttribute("id").slice(-1));
-          let payment_id = parseInt(payment_selected.getAttribute("id").slice(-1));
-
-          $('.cart-extra-field').each(function (index, element) {
-              let element_bind = parseInt(element.getAttribute("binded"));
-              if ([payment_id + 5, delivery_id + 2, 0].includes(element_bind)) {
-                  $(element).show();
-              } else {
-                  $(element).hide();
-              }
-          });
+    function change_delivery_variant(e) {
+      if (e) {
+        current_delivery = $(this).val();
       }
-
-      $("input[name=delivery_variant]").click(toggleExtraFields);
-      $("input[name=payment_variant]").click(toggleExtraFields);
-      toggleExtraFields();
+      if (current_delivery) {
+        payment_block.show().find("li").show();
+        payment_block
+        .show()
+        .find("input")
+        .each(function () {
+          var current_element = $(this);
+          if (!delivery_payment_mapping[current_delivery].includes(current_element.val())) {
+            current_element.prop("checked", false).parent().parent().hide();
+          }
+        });
+      }
+    }
+    $("input[name=delivery_variant]").click(change_delivery_variant);
+    change_delivery_variant(undefined);
   });
 })(jQuery);
