d4273c4bd287e33158c31bd91522f57a4543b270
Andrey Mayorov <aamayorov1@gmail.com>
2026-04-20 09:09:52 +0400

    task #19020

--- src/shared/libs/radioToggle/radioToggle.js
+++ src/shared/libs/radioToggle/radioToggle.js
@@ -5,8 +5,23 @@ export const initRadioToggle = () => {
   toggles.on('change', (event) => {
     const target = $(event.currentTarget)
     const id = target.attr('data-radio')
+    
     $('[data-radio-content]').addClass('d-none')
-    $(`[data-radio-content="${id}"]`).removeClass('d-none')
-    $(`[data-radio-content="*"]`).removeClass('d-none')
+    
+    $('[data-radio-content]').each(function() {
+      const $element = $(this)
+      const contentValue = $element.attr('data-radio-content')
+      
+      if (contentValue === '*') {
+        $element.removeClass('d-none')
+        return
+      }
+      
+      const contentIds = contentValue.split(',').map(item => item.trim())
+      
+      if (contentIds.includes(id)) {
+        $element.removeClass('d-none')
+      }
+    })
   })
 }
