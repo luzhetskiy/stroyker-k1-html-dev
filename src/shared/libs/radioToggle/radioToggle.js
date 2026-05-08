b31034c52a9a25bc02352236fbe27ec2bb0ea894
andrey <aamayorov1@gmail.com>
2026-05-08 10:49:26 +0400

    task #21782

--- src/shared/libs/radioToggle/radioToggle.js
+++ src/shared/libs/radioToggle/radioToggle.js
@@ -5,20 +5,25 @@ export const initRadioToggle = () => {
   toggles.on('change', (event) => {
     const target = $(event.currentTarget)
     const id = target.attr('data-radio')
-    
-    $('[data-radio-content]').addClass('d-none')
-    
-    $('[data-radio-content]').each(function() {
+    const group = target.attr('data-radio-group')
+
+    const contentSelector = group
+      ? `[data-radio-content][data-radio-group="${group}"]`
+      : '[data-radio-content]:not([data-radio-group])'
+
+    $(contentSelector).addClass('d-none')
+
+    $(contentSelector).each(function() {
       const $element = $(this)
       const contentValue = $element.attr('data-radio-content')
-      
+
       if (contentValue === '*') {
         $element.removeClass('d-none')
         return
       }
-      
+
       const contentIds = contentValue.split(',').map(item => item.trim())
-      
+
       if (contentIds.includes(id)) {
         $element.removeClass('d-none')
       }
